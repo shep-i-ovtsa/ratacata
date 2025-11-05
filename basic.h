e7387e64983b4689c857f0d218c93e603dd9cfcc
natalie <shepsemail1@gmail.com>
2025-11-05 04:13:08 -0800

    added .sleep();

--- basic.h
+++ basic.h
@@ -5,31 +5,33 @@
 #include <string>
 #include <cmath>
 #include <unordered_map>
-
+#include <chrono>
+#include <thread>
 class basic {
 public:
-
+    void init(const std::string& args);
     std::string format_label(std::string in, int x, int x2);
-    std::string identify_type(std::string type);
+    std::string identify_type(const std::string& type);
     int find_char(std::string in, char target);
 
-    std::string ascii_builder(std::string foreground, std::string background, std::string types);
-    std::string ascii_builder(int fore, int back, std::string type);
-    void print(std::string message);
+    std::string ascii_builder(const std::string& foreground, const std::string& background, const std::string& types);
+    std::string ascii_builder(int fore, int back, const std::string& type);
+    void print(const std::string& message);
+    void sleep(int ms);
+    void sleep(double seconds);
+    
+    void println(const std::string& message);
 
     
-    void println(std::string message);
+    void print_rich(const std::string& foreground, const std::string& background, const std::string& type, const std::string& message);
+    void print_rich(int foreground, int background, const std::string& type, const std::string& message);
 
     
-    void print_rich(std::string foreground, std::string background, std::string type, std::string message);
-    void print_rich(int foreground, int background, std::string type, std::string message);
-
-    
-    std::string prompt(std::string message);
+    std::string prompt(const std::string& message);
 
 
-    void set_clr(std::string foreground, std::string background, std::string type);
-    void set_clr(int foreground, int background, std::string type);
+    void set_clr(const std::string& foreground, const std::string& background, const std::string& type);
+    void set_clr(int foreground, int background, const std::string& type);
     
     void nwl();
 
@@ -51,14 +53,14 @@ public:
 
     std::string toStr(char message);
     int toInt(char message);
-    int toInt(std::string message);
+    int toInt(const std::string& message);
 
     std::string replace_char(std::string in, char start_char, char replace_char);
-    int count_chars(std::string in, char fing);
+    int count_chars(const std::string& in, char fing);
 
-    char toChar(std::string message);
+    char toChar(const std::string& message);
     char toChar(int message);
-    void replace(int x,int y,std::string message);
+    void replace(int x,int y,const std::string& message);
 };
 
 #endif
